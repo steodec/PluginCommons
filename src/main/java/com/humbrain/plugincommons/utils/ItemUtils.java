2f74c6662b0fca80f0241abbe4619a4cb9b3385b
Paul Tedesco <paul.tedesco@live.fr>
2025-07-30 20:02:29 +0200

    ✨ (ItemUtils): add French translation utility for Minecraft item/block ids ⬆️ (build.gradle): bump version to 1.0.4

--- src/main/java/com/humbrain/plugincommons/utils/ItemUtils.java
+++ src/main/java/com/humbrain/plugincommons/utils/ItemUtils.java
@@ -1,14 +1,55 @@
 package com.humbrain.plugincommons.utils;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ItemUtils {
+
+    private static final String TRANSLATION_URL = "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/refs/heads/1.21.4/assets/minecraft/lang/fr_fr.json";
+    private static final Map<String, String> TRANSLATION_MAP = new ConcurrentHashMap<>();
+    private static boolean loaded = false;
+
+    public static String translate(String id) {
+        try {
+            if (!loaded) {
+                loadTranslationMap();
+            }
+            String blockKey = "block.minecraft." + id.toLowerCase();
+            String itemKey = "item.minecraft." + id.toLowerCase();
+
+            // fallback
+            if (TRANSLATION_MAP.containsKey(blockKey)) {
+                return TRANSLATION_MAP.get(blockKey);
+            } else return TRANSLATION_MAP.getOrDefault(itemKey, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return id;
+        }
+    }
+
+    private static void loadTranslationMap() throws Exception {
+        URL url = new URL(TRANSLATION_URL);
+        try (InputStreamReader reader = new InputStreamReader(url.openStream(), StandardCharsets.UTF_8)) {
+            Gson gson = new Gson();
+            Map<String, String> map = gson.fromJson(reader, new TypeToken<Map<String, String>>() {
+            }.getType());
+            TRANSLATION_MAP.putAll(map);
+            loaded = true;
+        }
+    }
+
     @SuppressWarnings("deprecation")
     public String serializeItem(ItemStack item) {
         if (item == null || item.getType().isAir()) return null;
